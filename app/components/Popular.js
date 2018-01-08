a1a6e1ca8018e5ece095387696df6ad920c8d036
Rene Fernandez <renefs87@gmail.com>
2018-01-08 16:29:07 +0100

    Finish chapter 5

---
+++ app/components/Popular.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+class Popular extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedLanguage: 'All'
+        }
+
+        this.updateLanguage = this.updateLanguage.bind(this);
+    }
+
+    updateLanguage(lang) {
+        this.setState(function () {
+            return {
+                selectedLanguage: lang
+            }
+        })
+    }
+
+    render() {
+
+        var languages = ['All', 'Javascript', 'Ruby', 'Java', 'Python'];
+
+        return (
+            <ul className='languages'>
+                {languages.map(function (lang) {
+                    return (
+                        <li
+                            style={lang === this.state.selectedLanguage ? { color: '#d0021b' } : null}
+                            key={lang}
+                            onClick={this.updateLanguage.bind(null, lang)}>
+                            {lang}
+                        </li>
+                    )
+                }, this)
+                }
+            </ul >
+        );
+    }
+}
+
+module.exports = Popular;
