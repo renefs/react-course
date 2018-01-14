e3431e7f9151a9f2f42b4f70fa928f663ed2d51c
René Fernández <renefs87@gmail.com>
2018-01-14 17:20:33 +0100

    Finish chapter 8

---
+++ app/components/Battle.js
@@ -0,0 +1,15 @@
+var React = require('react');
+var Link = require('react-router-dom').Link;
+
+
+class Battle extends React.Component {
+    render() {
+        return (
+            <div className="battle-container" >
+                Battle
+            </div>
+        )
+    }
+}
+
+module.exports = Battle;
